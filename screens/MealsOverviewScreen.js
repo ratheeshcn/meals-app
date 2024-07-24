4e058a2e9ce14d9b6603439bace2ebc09c8668e5
Ratheesh CN <ratheeshcn87@gmail.com>
2024-07-24 19:18:06 +0400

    navigation and screen title

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,22 @@
 import { View, Text, StyleSheet, FlatList } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
 import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryID;
+
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({ title: categoryTitle });
+  }, [catId, navigation]);
 
   function renderedMealItem(itemData) {
     const item = itemData.item;
