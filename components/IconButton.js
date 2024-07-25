d4b6b4384a997f564906230dc75250ff9f20a5c5
Ratheesh CN <ratheeshcn87@gmail.com>
2024-07-25 13:59:25 +0400

    added a icon button on navigation bar

---
+++ components/IconButton.js
@@ -0,0 +1,24 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function IconButton({ icon, onPress, color }) {
+  return (
+    <Pressable>
+      <Ionicons
+        name={icon}
+        size={24}
+        color={color}
+        onPress={onPress}
+        style={({ pressed }) => pressed && styles.pressed}
+      />
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.7,
+  },
+});
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,12 +1,32 @@
 import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import { useLayoutEffect } from "react";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import SubTitle from "../components/MeaalDetail/SubTitle";
 import List from "../components/MeaalDetail/Lists";
+import IconButton from "../components/IconButton";
 
-function MealDetailsScreen({ route }) {
+function MealDetailsScreen({ route, navigation }) {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log("pressed");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
+      },
+    });
+  }, [navigation]);
 
   return (
     <ScrollView style={styles.root}>
